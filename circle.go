ff230e7cee90759f4d99100f0b1e56c8d48111a1
clambin <christophe.lambin@gmail.com>
2021-12-05 17:36:04 +0100

    feat: catch demo

--- circle.go
+++ circle.go
@@ -7,27 +7,10 @@ import (
 
 func NewCircle(options ObjectOptions) Object {
 	options.BodyOptions.Type = chipmunk.ShapeType_Circle
-
 	shape := chipmunk.NewCircle(
 		vect.Vector_Zero,
 		options.BodyOptions.CircleOptions.Radius,
 	)
-	shape.SetElasticity(options.BodyOptions.Elasticity)
-	shape.SetFriction(options.BodyOptions.Friction)
 
-	var body *chipmunk.Body
-	if options.BodyOptions.StaticBody {
-		body = chipmunk.NewBodyStatic()
-	} else {
-		body = chipmunk.NewBody(options.BodyOptions.Mass, shape.Moment(float32(options.BodyOptions.Mass)))
-	}
-	body.AddShape(shape)
-	body.SetPosition(options.BodyOptions.Position)
-	body.SetVelocity(float32(options.BodyOptions.Velocity.X), float32(options.BodyOptions.Velocity.Y))
-	body.SetAngle(options.BodyOptions.Angle)
-
-	return Object{
-		body:    body,
-		options: options,
-	}
+	return NewObjectWithShape(shape, options)
 }
